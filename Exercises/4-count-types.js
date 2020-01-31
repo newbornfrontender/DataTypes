e0f1d9c98f3a90f02e4225976a52045a738d9f15
newbornfrontender <newbornfrontender@gmail.com>
2020-01-31 15:48:08 +0300

    feat(2-4): solve ex

--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,49 @@
 'use strict';
 
-const countTypesInArray = null;
+// const f = () => console.log('hello');
+// const arr = [
+//   true,
+//   'User',
+//   f,
+//   2,
+//   false,
+//   null,
+//   'Str',
+//   undefined,
+//   26817637698172938123,
+//   { a: '123' },
+//   false,
+//   ['1', 2, null],
+// ];
+
+const isArr = value => Array.isArray(value) && 'array';
+const isNull = value => value === null && 'null';
+
+const countTypesInArray = arr => {
+  const keys = {};
+
+  for (const item of arr) {
+    const type = isArr(item) || isNull(item) || typeof item;
+    const key = keys[type] || 0;
+
+    keys[type] = key + 1;
+  }
+
+  return keys;
+};
+
+// const countTypesInArray = arr => {
+//   const keys = {};
+
+//   for (const item of arr) {
+//     const type = typeof item;
+//     const key = keys[type] || 0;
+
+//     keys[type] = key + 1;
+//   }
+
+//   return keys;
+// };
+
 
 module.exports = { countTypesInArray };
